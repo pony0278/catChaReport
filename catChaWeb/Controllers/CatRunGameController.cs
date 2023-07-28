c8d74bb9ae1fb95e60b87a608562521c8021158c
Allun <ocarina51006@gmail.com>
2023-07-28 11:54:59 +0800

    no message

---
+++ catChaWeb/Controllers/CatRunGameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace catChaWeb.Controllers
+{
+    public class CatRunGameController : Controller
+    {
+        // GET: CatRunGame
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ catChaWeb/Controllers/GameMainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace catChaWeb.Controllers
+{
+    public class GameMainController : Controller
+    {
+        // GET: GameMain
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
